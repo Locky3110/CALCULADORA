2e3cc57437533720b7b5211a80baeeeeeec06fe9
Locky3110 <Lockygpt@gmail.com>
2025-10-19 01:32:43 -0500

    Cambios

--- calculadora/calculos.py
+++ calculadora/calculos.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 import math
 
-app = Blueprint('calculos_DE', __name__)
+app = Blueprint('calculos_DE', __name__,template_folder='calculadora/templates')
 
 @app.route('/')
 def inicio():
